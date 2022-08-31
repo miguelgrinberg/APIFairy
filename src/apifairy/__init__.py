73437d630f5a5889b0a7490e8a1747b4e5ae021c
Adam Verner <34060105+AdamVerner@users.noreply.github.com>
2022-08-31 11:13:32 +0200

    add decorator to __init__

--- src/apifairy/__init__.py
+++ src/apifairy/__init__.py
@@ -1,4 +1,4 @@
 from .core import APIFairy  # noqa: F401
 from .decorators import authenticate, arguments, body, response, \
-    other_responses  # noqa: F401
+    other_responses, other_response  # noqa: F401
 from .fields import FileField  # noqa: F401
